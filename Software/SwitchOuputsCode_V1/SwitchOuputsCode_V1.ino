482c6751bbe1da1c4b282c0e19fa1694409fd639
kjans123 <kj123@duke.edu>
2018-11-02 01:09:31 -0400

    wrote working code to get light to switch brigtnesses & flash (close #12)

--- Software/SwitchOuputsCode_V1/SwitchOuputsCode_V1.ino
+++ Software/SwitchOuputsCode_V1/SwitchOuputsCode_V1.ino
@@ -7,31 +7,59 @@ int currentState;
 int lastState;
 int pwmVal = 0;
 int operating_state=0;
+bool LED_ON = false;
+double turn_light_off_time=0;
+double turn_light_on_time = 0;
+double flash_count = 0;
+double button_pressed_time=0;
 volatile bool button_pushed = false;
 
+double on_off_time = (pow(FLASH_RATE_Hz,-1)*0.5)*1000;
 
 void setup() {
   pinMode(outPin, OUTPUT);
+
   pinMode(inPin, INPUT);
   attachInterrupt(digitalPinToInterrupt(inPin), button_pushed_func, RISING);
+  Serial.begin(9600);
 }
 
 void loop() {
   check_for_button_press();
   cycle_light();
+
+
 }
 
 
 void button_pushed_func(){
   button_pushed = true;
+  button_pressed_time = millis();
 }
 
 void flash_the_light(){
-  double on_off_time = (pow(FLASH_RATE_Hz,-1)*0.5)*1000;
-  analogWrite(outPin,PWM_MAX);
-  delay(on_off_time);
-  analogWrite(outPin,0);
-  delay(on_off_time);
+  if (flash_count == 0){
+      if (millis()-button_pressed_time > on_off_time && LED_ON == true){
+        analogWrite(outPin,0);
+        turn_light_off_time = millis();
+        LED_ON = false;
+      }
+  }
+
+  else if (flash_count >0) {
+    if (LED_ON == false && (millis()-turn_light_off_time) > on_off_time){
+        analogWrite(outPin,255);
+        turn_light_on_time = millis();
+        LED_ON = true;
+    }
+    else if (LED_ON == true && (millis()-turn_light_on_time) > on_off_time){
+        analogWrite(outPin, 0);
+        turn_light_off_time = millis();
+        LED_ON=false;
+    }
+
+  }
+flash_count = 1 + flash_count;
 }
 
 
@@ -43,31 +71,44 @@ void check_for_button_press(){
     currentState = digitalRead(inPin);
     if (currentState == 1 && lastState == 0){
       operating_state = operating_state + 1;
-      button_pushed = false;
+
     }
   }
+ button_pushed = false;
 }
 
-void cycle_light(){      
+void cycle_light(){
   switch(operating_state){
-    case 0: 
+    case 0:
       pwmVal = 0;
+      LED_ON = false;
+      analogWrite(outPin, pwmVal);
       break;
     case 1:
       pwmVal = int(PWM_MAX/4);
+      LED_ON = true;
+      analogWrite(outPin, pwmVal);
       break;
     case 2:
       pwmVal = int(PWM_MAX/2);
+      LED_ON = true;
+      analogWrite(outPin, pwmVal);
       break;
     case 3:
       pwmVal = PWM_MAX;
+      LED_ON = true;
+      analogWrite(outPin, pwmVal);
       break;
     case 4:
       flash_the_light();
       break;
   case 5:
       operating_state = 0;
+      LED_ON = false;
+      flash_count=0;
+      pwmVal = 0;
+      analogWrite(outPin, pwmVal);
       break;
 }
-analogWrite(outPin, pwmVal);  
+
 }
